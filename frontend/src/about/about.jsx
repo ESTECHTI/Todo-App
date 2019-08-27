df1a04e0b4f64e038555ff89b194d1061fa0ee01
Elisandro Silva de Oliveira <elisandro.oliveira@pampa.compasso>
2019-08-27 13:17:52 -0300

    My first commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader';
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós"></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolo sit amet...</p>
+        <h2>Missão e visão</h2>
+        <p>Lorem ipsum dolo sit amet...</p>
+        <h2>Impressa</h2>
+        <p>Lorem ipsum dolo sit amet...</p>
+    </div>
+)
---
+++ frontend/src/template/pageHeader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default props => (
+    <header className='page-header'>
+        <h2>{props.name} <smal> {props.small}</smal></h2>
+    </header>
+)
